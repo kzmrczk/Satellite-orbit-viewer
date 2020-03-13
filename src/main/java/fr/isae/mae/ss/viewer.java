f4d614ef756652f8023598032dcbb2e83406f247
mizix1900 <60691652+mizix1900@users.noreply.github.com>
2020-03-13 09:03:44 +0100

    Update viewer.java

--- src/main/java/fr/isae/mae/ss/viewer.java
+++ src/main/java/fr/isae/mae/ss/viewer.java
@@ -1,5 +1,6 @@
 package fr.isae.mae.ss;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 import gov.nasa.worldwind.WorldWind;
@@ -11,12 +12,10 @@ import gov.nasa.worldwind.render.Material;
 import gov.nasa.worldwind.render.Path;
 import gov.nasa.worldwind.render.ShapeAttributes;
 import gov.nasa.worldwind.util.BasicDragger;
-import gov.nasa.worldwind.util.WWUtil;
 import gov.nasa.worldwindx.examples.ApplicationTemplate;
 
 public class viewer extends ApplicationTemplate {
-	static double a = 1;
-	static double[] in = new double[6];
+
 	static ArrayList<Position> pathPositions = new ArrayList<Position>();
 	
 	@SuppressWarnings("serial")
@@ -24,37 +23,37 @@ public class viewer extends ApplicationTemplate {
     {
         public DisplayPaths()
         {
-            super(false, false, false);
+        	// Specify whether to include: (Status bar, Layer panel, Statistics panel)
+            super(true, true, false);
             // Add a dragger to enable shape dragging
             this.getWwd().addSelectListener(new BasicDragger(this.getWwd()));
             RenderableLayer layer = new RenderableLayer();
 
-            // Create a path with more than two positions and closed.
+            // Create a path with more than two positions and closed
             ArrayList<Position> pathPositions = OrbitGenerator.pathPositions(Dialogging.main());
             Path path = new Path(pathPositions);
+            // Set altitude mode. Possible modes: ABSOLUTE, RELATIVE_TO_GROUND, CLAMP_TO_GROUND
             path.setAltitudeMode(WorldWind.ABSOLUTE);
-            path.setExtrude(false);
+            // Set path type. Possible types: LINEAR, GREAT_CIRCLE, RHUMB_LINE
             path.setPathType(AVKey.LINEAR);
 
-            // Create and set an attribute bundle.
+            // Create and set an attribute bundle
             ShapeAttributes attrs = new BasicShapeAttributes();
-            attrs.setOutlineMaterial(new Material(WWUtil.makeRandomColor(null)));
-            attrs.setInteriorMaterial(new Material(WWUtil.makeRandomColor(null)));
-            attrs.setOutlineWidth(2);
+            // Set color of the orbit
+            attrs.setOutlineMaterial(new Material(Color.RED));
+            // Set width of the orbit's line
+            attrs.setOutlineWidth(5);
+            // Assign attributes to the path
             path.setAttributes(attrs);
-
+            // Add the path to the layer
             layer.addRenderable(path);
-
-            // Add the layer to the model.
+            // Add the layer to the model
             insertBeforeCompass(getWwd(), layer);
-        }
-        
-        
+        }        
     }
 	
 	public static void main(String[] args) {
-		//in = Dialogging.main();
-		
-		ApplicationTemplate.start("World Wind Paths", DisplayPaths.class);
+		// Display the orbit model
+		start("OrbitViewer1000", DisplayPaths.class);
 	}
 }
