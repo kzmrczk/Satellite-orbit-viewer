94f85c3781b5b5b24d705cce9ddaadea47fb3066
mizix1900 <60691652+mizix1900@users.noreply.github.com>
2020-03-14 03:42:28 +0100

    Create AbsoluteFilePath.java
    Class that helps with loading Orekit data

---
+++ src/main/java/fr/isae/mae/ss/AbsoluteFilePath.java
@@ -0,0 +1,29 @@
+package fr.isae.mae.ss;
+
+import java.io.File;
+
+/**
+ * 
+ * @author mkyong at https://mkyong.com/java/how-to-get-the-filepath-of-a-file-in-java/
+ * modified and commented by: Dawid Kazimierczak
+ *
+ * Method getPath is used to find the location of a specific file. It takes
+ * the filename as an argument and returns its path.
+ * In case of OrbitViewer1000 the method is used to find "orekit-data-master.zip"
+ */
+
+public class AbsoluteFilePath
+{
+    public static String getPath(String args)
+    {	
+    	// Creating a File object with a specific name
+    	File orekit = new File(args);
+		// Getting the absolute path to that file
+		String absolutePath = orekit.getAbsolutePath();
+		// Cutting the part after the last separator from the path
+		String filePath = absolutePath.
+			     substring(0,absolutePath.lastIndexOf(File.separator));
+			
+		return filePath;    	
+    }
+}
